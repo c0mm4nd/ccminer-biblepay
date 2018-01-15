0d5e136c8885bed79d2655680de99a91ef906f00
Command M <maoxs2@163.com>
2018-01-16 07:26:57 +0800

    Just modified several files, without compiled

--- x11/x11.cu
+++ x11/x11.cu
@@ -13,6 +13,7 @@ extern "C"
 #include "sph/sph_shavite.h"
 #include "sph/sph_simd.h"
 #include "sph/sph_echo.h"
+#include "sph/sph_biblepay.h"
 
 #include "miner.h"
 }
@@ -89,66 +90,41 @@ inline void x11hash(void *state, const void *input)
 
     unsigned char hash[64];
 
-    sph_blake512_init(&ctx_blake);
     // ZBLAKE;
+    sph_blake512_init(&ctx_blake);
     sph_blake512 (&ctx_blake, input, 80);
     sph_blake512_close(&ctx_blake, (void*) hash);
     
-    sph_bmw512_init(&ctx_bmw);
     // ZBMW;
+    sph_bmw512_init(&ctx_bmw);
     sph_bmw512 (&ctx_bmw, (const void*) hash, 64);
     sph_bmw512_close(&ctx_bmw, (void*) hash);
 
-    sph_groestl512_init(&ctx_groestl);
     // ZGROESTL;
+    sph_groestl512_init(&ctx_groestl);
     sph_groestl512 (&ctx_groestl, (const void*) hash, 64);
     sph_groestl512_close(&ctx_groestl, (void*) hash);
 
-    sph_skein512_init(&ctx_skein);
     // ZSKEIN;
+    sph_skein512_init(&ctx_skein);
     sph_skein512 (&ctx_skein, (const void*) hash, 64);
     sph_skein512_close(&ctx_skein, (void*) hash);
 
-    sph_jh512_init(&ctx_jh);
     // ZJH;
+    sph_jh512_init(&ctx_jh);
     sph_jh512 (&ctx_jh, (const void*) hash, 64);
     sph_jh512_close(&ctx_jh, (void*) hash);
 
-    sph_keccak512_init(&ctx_keccak);
     // ZKECCAK;
+    sph_keccak512_init(&ctx_keccak);
     sph_keccak512 (&ctx_keccak, (const void*) hash, 64);
     sph_keccak512_close(&ctx_keccak, (void*) hash);
 
-    sph_luffa512_init(&ctx_luffa);
-    // ZLUFFA;
-    sph_luffa512 (&ctx_luffa, (const void*) hash, 64);
-    sph_luffa512_close (&ctx_luffa, (void*) hash);
+    // BIBLEPAY
+    sph_biblepay512_init(&ctx_keccak);
+    sph_biblepay512 (&ctx_keccak, (const void*) hash, 64);
+    sph_biblepay512_close(&ctx_keccak, (void*) hash);
 
-#if 1
-    sph_cubehash512_init(&ctx_cubehash);
-    // ZCUBEHASH;
-    sph_cubehash512 (&ctx_cubehash, (const void*) hash, 64);
-    sph_cubehash512_close(&ctx_cubehash, (void*) hash);
-#endif
-
-#if 1
-    sph_shavite512_init(&ctx_shavite);
-    // ZSHAVITE;
-    sph_shavite512 (&ctx_shavite, (const void*) hash, 64);
-    sph_shavite512_close(&ctx_shavite, (void*) hash);
-#endif
-
-    sph_simd512_init(&ctx_simd);
-    // ZSIMD
-    sph_simd512 (&ctx_simd, (const void*) hash, 64);
-    sph_simd512_close(&ctx_simd, (void*) hash);
-
-#if 1
-    sph_echo512_init(&ctx_echo);
-    // ZECHO
-    sph_echo512 (&ctx_echo, (const void*) hash, 64);
-    sph_echo512_close(&ctx_echo, (void*) hash); 
-#endif
 
     memcpy(state, hash, 32);
 }
